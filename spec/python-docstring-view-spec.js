2d880ce5c5525987b3b0fe5121f27240e708b216
Filip Todic <filip.todic@styria.hr>
2017-12-09 14:05:47 +0100

    Generate the package structure

---
+++ spec/python-docstring-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PythonDocstringView from '../lib/python-docstring-view';
+
+describe('PythonDocstringView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
